3eee218461a62034f1bd13b9a596f6dc93a34399
nickollay <nickollaysokolov@gmail.com>
2020-01-23 22:01:09 +0300

    Scaffold Microposts with content and user_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-	def hello
-		render html: 'Hello world!'
-	end
+	# def hello
+	# 	render html: 'Hello world!'
+	# end
 
 	def goodbuy
 		render html: 'Goodbuy, World! I will be back, soon!!!'
